c1e7d2fc2ccff9312c2bb8f63dfd0e8d16096bf5
Sam Joan Roque-Worcel <sroque-worcel@gitlab.com>
2025-12-04 11:21:53 +1300

    Refactor part 1, solve part 2.

--- 3/main.go
+++ 3/main.go
@@ -26,49 +26,70 @@ func parse_inputs(filename string) ([]string, error) {
 	return banks, nil
 }
 
+func index(bank string) map[int][]int {
+	i := make(map[int][]int)
+
+	for pos, char := range bank {
+		val, err := strconv.Atoi(string(char)); if err != nil { panic(err) }
+		i[val] = append(i[val], pos)
+	}
+
+	return i
+}
+
+func solve(bank map[int][]int, bankSize int, desiredLen int) int {
+	solution := []int{}
+	lastPos := -1
+	for i := 0; i < desiredLen; i++ {
+		foundSuitableDigit := false
+		for highestNum := 9; highestNum >= 1; highestNum-- {
+			positions := bank[highestNum]
+			if len(positions) > 0 {
+				for _, pos := range positions {
+					lastPossiblePos := bankSize - (desiredLen - len(solution)) 
+					// fmt.Printf("nb: %d, pos: %d, lastPos: %d, lastPossiblePos: %d, bankSize: %d\n", highestNum, pos, lastPos, lastPossiblePos, bankSize)
+					if pos > lastPos && pos <= lastPossiblePos {
+						solution = append(solution, highestNum)
+						foundSuitableDigit = true
+						lastPos = pos
+						break
+					}
+				}
+			}
+
+			if foundSuitableDigit {
+				break
+			}
+		}
+	}
+
+	solutionStr := ""
+	for _, i := range solution {
+		solutionStr += strconv.Itoa(i)
+	}
+	
+	numericValue, err := strconv.Atoi(solutionStr); if err != nil {panic(err)}
+
+	return numericValue
+}
+
 func solve_part1(banks []string) int {
 	total := 0
 	for _, bank := range banks {
-		bankLen := len(bank)
-		firstValue := -1
-		firstValuePos := -1
-		for targetNumber := 9; targetNumber >= 1; targetNumber-- {
-			// -1 for array offset, -1 for the fact that we can't have the first char be at the last spot.
-			for pos:= 0; pos <= bankLen - 2; pos++ {
-				valueInBank, err := strconv.Atoi(string(bank[pos])); if err != nil { panic(err) }
-				if valueInBank == targetNumber {
-					firstValue = valueInBank
-					firstValuePos = pos
-					break
-				}
-			}
+		bankIndex := index(bank)
+		solution := solve(bankIndex, len(bank), 2)
+		total += solution
+	}
 
-			if firstValue != -1 {
-				break
-			}
-		}
+	return total
+}
 
-		secondValue := -1
-		for targetNumber := 9; targetNumber >= 1; targetNumber-- {
-			for pos := bankLen - 1; pos > firstValuePos; pos-- {
-				valueInBank, err := strconv.Atoi(string(bank[pos])); if err != nil { panic(err) }
-				if valueInBank == targetNumber {
-					secondValue = valueInBank
-					break
-				}
-			}
-
-			if secondValue != -1 {
-				break
-			}
-		}
-
-		firstValueStr := strconv.Itoa(firstValue)
-		secondValueStr := strconv.Itoa(secondValue)
-
-		jolts, err := strconv.Atoi(firstValueStr + secondValueStr); if err != nil { panic(err) }
-
-		total += jolts
+func solve_part2(banks []string) int {
+	total := 0
+	for _, bank := range banks {
+		bankIndex := index(bank)
+		solution := solve(bankIndex, len(bank), 12)
+		total += solution
 	}
 
 	return total
@@ -81,6 +102,8 @@ func main() {
 	}
 
 	part1 := solve_part1(items)
+	part2 := solve_part2(items)
 	
 	fmt.Printf("Result %d\n", part1)
+	fmt.Printf("Result %d\n", part2)
 }
